38f9688c22b24ea5a0bcc712aaae8527c9c1fa61
dionlahh <dion.toh19@gmail.com>
2024-04-09 15:09:51 +0800

    updates to about page

--- doggle-webapp/src/About Page/About.js
+++ doggle-webapp/src/About Page/About.js
@@ -2,12 +2,33 @@ import React from "react";
 
 function About() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <h2>About page</h2>
-        </div>
+    <div className="container">
+      <header>
+        <h1 className="text-center mt-5">About Us</h1>
       </header>
+      <div className="row mt-5">
+        <div className="col-md-6">
+          <h2>Lorem ipsum</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
+            augue velit. Donec vitae tortor consequat, facilisis nulla at,
+            hendrerit dolor.
+          </p>
+          <p>
+            Nullam sollicitudin convallis eros, nec convallis urna facilisis in.
+            Curabitur nec augue sapien. Fusce quis diam ac dolor feugiat auctor.
+          </p>
+        </div>
+        <div className="col-md-6">
+          <h2>Our Team</h2>
+          <p>Meet the amazing team behind this project.</p>
+          <ul>
+            <li>Dion Toh Siyong</li>
+            <li>Tan Yue Jun</li>
+            <li>Jewel Wee</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
